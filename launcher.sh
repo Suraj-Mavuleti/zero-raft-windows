794ffe1bc3f794ff8c95babd5b5bf0b439a94854
Suraj Mavuleti <surajmavuleti@gmail.com>
2026-09-06 20:17:36 +0530

    Feat(v8): Visual Overhaul using CustomTkinter Modern UI Framework

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# AUTO-UPDATER
 cd /home/suraj/.gemini/antigravity/scratch/ultimate_suite/zero-raft-windows
 git pull origin main --quiet
-python3 zero_raft_gui.py
+/home/suraj/.gemini/antigravity/scratch/v8_env/bin/python3 zero_raft_gui.py
